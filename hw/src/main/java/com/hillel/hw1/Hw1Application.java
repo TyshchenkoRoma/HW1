0246621b9f95450cbca2a5e25a57c87f40096258
Roman Tyshchenko <rtyshchenko@griddynemics.com>
2021-02-28 17:41:48 +0200

    finish

--- hw/src/main/java/com/hillel/hw1/Hw1Application.java
+++ hw/src/main/java/com/hillel/hw1/Hw1Application.java
@@ -1,13 +1,18 @@
 package com.hillel.hw1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class Hw1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Hw1Application.class, args);
+		log.trace("A TRACE Message");
+		log.debug("A DEBUG Message");
+		log.info("An INFO Message");
+		log.warn("A WARN Message");
+		log.error("An ERROR Message");
 	}
-
 }
